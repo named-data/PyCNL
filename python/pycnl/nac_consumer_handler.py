fdf0487bfa2e772f9b8ea9d5fd84491bc03958af
Jeff Thompson <jefft0@remap.ucla.edu>
2016-05-18 12:18:35 -0700

    Added initial NacConsumerHandler

--- python/pycnl/nac_consumer_handler.py
+++ python/pycnl/nac_consumer_handler.py
@@ -17,16 +17,23 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # A copy of the GNU Lesser General Public License is in the file COPYING.
 
-from pycnl import nac_consumer_handler, namespace, segment_stream, segmented_content
-__all__ = ['nac_consumer_handler', 'namespace', 'segment_stream', 'segmented_content']
+"""
+This module defines the NacConsumerHandler class which uses a Name-based Access
+Control Consumer to automatically decrypt Data packets which are attached to a
+Namespace node.
+"""
 
-import sys as _sys
+from pyndn.encrypt import Consumer
 
-try:
-    from pycnl.nac_consumer_handler import *
-    from pycnl.namespace import *
-    from pycnl.segment_stream import *
-    from pycnl.segmented_content import *
-except ImportError:
-    del _sys.modules[__name__]
-    raise
+class NacConsumerHandler(object):
+    """
+    Create a NacConsumerHandler object to attach to the given Namespace. This
+    holds an internal NAC Consumer with the given values. This uses the Face
+    which must already be set for the Namespace (or one of its parents).
+    """
+    def __init__(self, namespace, keyChain, groupName, consumerName, database):
+        # TODO: What is the right way to get access to the Face?
+        face = namespace._getFace()
+        self._consumer = Consumer(
+          face, keyChain, groupName, consumerName, database)
+
