1f38f56a62fcdf455132ee1fbbc5315e96bab5a0
Jeff Thompson <jefft0@remap.ucla.edu>
2018-06-19 15:36:16 -0700

    examples: In test_segmented, change the demo host to memoria.

--- examples/test_segmented.py
+++ examples/test_segmented.py
@@ -32,8 +32,7 @@ def dump(*list):
     print(result)
 
 def main():
-    # Connect to the demo host at memoria.ndn.ucla.edu .
-    face = Face("128.97.98.8")
+    face = Face("memoria.ndn.ucla.edu")
     page = Namespace("/ndn/edu/ucla/remap/demo/ndn-js-test/named-data.net/project/ndn-ar2011.html/%FDT%F7n%9E")
     page.setFace(face)
 
