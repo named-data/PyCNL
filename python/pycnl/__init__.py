a7210798421c21c2eb30cfc5cc74bd7cee0b92b5
Jeff Thompson <jefft0@remap.ucla.edu>
2018-10-08 13:36:44 -0700

    Removed unused NacConsumerHandler (from NAC version 1).

--- python/pycnl/__init__.py
+++ python/pycnl/__init__.py
@@ -17,15 +17,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # A copy of the GNU Lesser General Public License is in the file COPYING.
 
-from pycnl import nac_consumer_handler, namespace, segment_stream_handler
+from pycnl import namespace, segment_stream_handler
 from pycnl import segmented_object_handler
-__all__ = ['nac_consumer_handler', 'namespace', 'segment_stream_handler',
+__all__ = ['namespace', 'segment_stream_handler',
            'segmented_object_handler']
 
 import sys as _sys
 
 try:
-    from pycnl.nac_consumer_handler import *
     from pycnl.namespace import *
     from pycnl.segment_stream_handler import *
     from pycnl.segmented_object_handler import *
